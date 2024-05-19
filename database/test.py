272e45ba5ad0bd5e39b2b0ac766921c241ba0aab
diominvd <diomin.vd13@icloud.com>
2024-05-19 19:47:37 +0300

    Commit.

--- database/test.py
+++ database/test.py
@@ -4,11 +4,11 @@ from core.secrets import DATABASE
 
 class Database:
     host = DATABASE["host"]
-    user = DATABASE["user"]
+    username = DATABASE["username"]
     password = DATABASE["password"]
     scheme = DATABASE["scheme"]
 
-    connection = connect(host=host, user=user, password=password, database=scheme)
+    connection = connect(host=host, user=username, password=password, database=scheme)
     cursor = connection.cursor()
 
 
@@ -16,5 +16,36 @@ class Table(Database):
     def __init__(self):
         pass
 
-    def create(self, table: tuple[str, dict]) -> bool:
-        pass
+    def create(self, table_name: str, columns: dict[str, str]) -> None:
+        columns: list = [f"{key} {value}" for key, value in columns.items()]
+
+        query: str = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)})"
+        self.cursor.execute(query)
+        self.connection.commit()
+        return None
+
+    def insert(self, table_name: str, data: dict[str, any]) -> None:
+        print(', '.join([column for column in data.keys()]))
+        print(', '.join([str(value) for value in data.values()]))
+        query: str = f"INSERT INTO {table_name} ({', '.join([column for column in data.keys()])}) VALUES ({', '.join([value for value in data.values()])})"
+        self.cursor.execute(query)
+        self.connection.commit()
+        return None
+
+
+class TestTable(Table):
+    def __init__(self):
+        super().__init__()
+        self.name: str = "test_table"
+        self.columns: dict = {
+            "username": "VARCHAR(255)",
+            "password": "VARCHAR(255)",
+            "number": "INT"
+        }
+        self.create(self.name, self.columns)
+
+
+test = TestTable()
+data = {"username": "John", "password": "1", "number": 100}
+print(', '.join(data.keys()))
+test.insert("test_table", data)
